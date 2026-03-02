907c8f96a1a178dbd33b609e551f0fe64baa1724
carolinanoemi <cnoemimartinez@gmail.com>
2026-03-02 11:16:58 +0100

    update OnlyCounytsActiveBookingsTestData

--- HotelBooking.UnitTests/TestData/OnlyCountsActiveBookingsTestData.cs
+++ HotelBooking.UnitTests/TestData/OnlyCountsActiveBookingsTestData.cs
@@ -1,78 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using HotelBooking.Core;
 
 namespace HotelBooking.UnitTests.TestData
 {
-    public static class SomeDatesFullyOccupiedTestData
+    public class OnlyCountsActiveBookingsTestData
     {
         public static IEnumerable<object[]> Data =>
-        new List<object[]>
-        {
-            // Scenario A: kun nogle dage er fully occupied
-            // Hotellet har 2 rooms (Id 1 og 2)
-            // Room 1 er booket hele perioden
-            // Room 2 er booket kun midten (only those middle dates er fully occupied)
-            new object[]
+            new List<object[]>
             {
-                DateTime.Today.AddDays(10), // startDate
-                DateTime.Today.AddDays(13), // endDate
-
-                new List<Booking> // bookings
+                new object[]
                 {
-                    new Booking
+                    DateTime.Today.AddDays(10),
+                    DateTime.Today.AddDays(12),
+
+                    new List<Booking>
                     {
-                        Id = 1, RoomId = 1, IsActive = true,
-                        StartDate = DateTime.Today.AddDays(10),
-                        EndDate = DateTime.Today.AddDays(13)
+                        // Room 1 aktiv hele perioden
+                        new Booking
+                        {
+                            RoomId = 1,
+                            StartDate = DateTime.Today.AddDays(10),
+                            EndDate = DateTime.Today.AddDays(12),
+                            IsActive = true
+                        },
+
+                        // Room 2 INAKTIV hele perioden (må ikke tælle)
+                        new Booking
+                        {
+                            RoomId = 2,
+                            StartDate = DateTime.Today.AddDays(10),
+                            EndDate = DateTime.Today.AddDays(12),
+                            IsActive = false
+                        },
+
+                        // Room 2 aktiv kun på dag 11 (så kun dag 11 bliver fully occupied)
+                        new Booking
+                        {
+                            RoomId = 2,
+                            StartDate = DateTime.Today.AddDays(11),
+                            EndDate = DateTime.Today.AddDays(11),
+                            IsActive = true
+                        }
                     },
-                    new Booking
+
+                    new List<DateTime>
                     {
-                        Id = 2, RoomId = 2, IsActive = true,
-                        StartDate = DateTime.Today.AddDays(11),
-                        EndDate = DateTime.Today.AddDays(12)
+                        DateTime.Today.AddDays(11)
                     }
-                },
-
-                new List<DateTime> // expected fully occupied dates
-                {
-                    DateTime.Today.AddDays(11),
-                    DateTime.Today.AddDays(12)
                 }
-            },
-
-            // Scenario B: kun én dag er fully occupied
-            new object[]
-            {
-                DateTime.Today.AddDays(20),
-                DateTime.Today.AddDays(22),
-
-                new List<Booking>
-                {
-                    // Room 1 booket alle dage 20-22
-                    new Booking
-                    {
-                        Id = 3, RoomId = 1, IsActive = true,
-                        StartDate = DateTime.Today.AddDays(20),
-                        EndDate = DateTime.Today.AddDays(22)
-                    },
-                    // Room 2 booket kun dag 21
-                    new Booking
-                    {
-                        Id = 4, RoomId = 2, IsActive = true,
-                        StartDate = DateTime.Today.AddDays(21),
-                        EndDate = DateTime.Today.AddDays(21)
-                    }
-                },
-
-                new List<DateTime>
-                {
-                    DateTime.Today.AddDays(21)
-                }
-            }
-        };
+            };
     }
-
-
 }
-
